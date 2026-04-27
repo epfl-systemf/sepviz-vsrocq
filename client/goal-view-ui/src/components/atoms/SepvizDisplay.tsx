c4edc70fcc38ebfa590c5099bd072cfaf39b6504
Yawen Guan <yawen.guan@proton.me>
2026-04-27 22:39:51 +0200

    Move animation setup to sepviz & dependencies cleanup
    The previous animation speed issue was caused by inconsistent d3
    libraries (one from the sepviz library and one from vsrocq). The issue
    is resolved by moving the animation logic to sepviz and removing d3
    dependencies in vsrocq.

--- client/goal-view-ui/src/components/atoms/SepvizDisplay.tsx
+++ client/goal-view-ui/src/components/atoms/SepvizDisplay.tsx
@@ -1,7 +1,5 @@
 import { FunctionComponent, useRef, useEffect, RefObject } from 'react';
-import { Render, ExtHTMLElement } from 'sepviz';
-import { transition } from 'd3-transition';
-import { easeCubicInOut } from 'd3-ease';
+import { Render } from 'sepviz';
 import "sepviz/sepviz.css";
 
 interface SepvizDisplayProps {
@@ -15,13 +13,11 @@ interface SepvizDisplayProps {
 const SepvizDisplay: FunctionComponent<SepvizDisplayProps> = (props) => {
   const {goalText, ppRef, render, onFallback, animate} = props;
   const hostRef = useRef<HTMLDivElement>(null);
-  const prevDotsRef = useRef<Map<string, string | undefined>>(new Map());
 
   useEffect(() => {
     const host = hostRef.current;
     if (!host || !goalText) return;
     host.innerHTML = '';
-
     try {
       render.render(goalText, host, animate);
     } catch (e) {
@@ -29,26 +25,7 @@ const SepvizDisplay: FunctionComponent<SepvizDisplayProps> = (props) => {
       onFallback();
       return;
     }
-
-    if(!animate) return;
-    ['PRE', 'POST'].forEach((stream) => {
-      const vizNode = host.querySelector(`.sep-visualization.sep-stream-${stream}`); 
-      const svgNode = vizNode?.querySelector<ExtHTMLElement>('.sep-svg');
-      const gviz = svgNode?.__graphviz__;
-      const currDot = svgNode?.dot;
-      const prevDot = prevDotsRef.current.get(stream);
-      prevDotsRef.current.set(stream, currDot);
-
-      if (!gviz || !currDot || !prevDot || prevDot === currDot) return;
-
-      gviz.renderDot(prevDot);
-      requestAnimationFrame(() => {
-        gviz
-          // FIXME: except for the first triggered animation, the transition turns out to be much faster
-          .transition(() => (transition().duration(1000).ease(easeCubicInOut)) as any)
-          .renderDot(currDot);
-      });
-    });
+    if(animate) render.animate(host);
   }, [goalText, ppRef, render, onFallback]);
 
   return <div ref={hostRef} className="sepviz-display" />;
--- client/goal-view-ui/vite.config.ts
+++ client/goal-view-ui/vite.config.ts
@@ -5,9 +5,6 @@ import peggyPlugin from './vite-plugin-peggy';
 // https://vitejs.dev/config/
 export default defineConfig( ({mode}) => ({
   plugins: [react(), peggyPlugin()],
-  resolve: {
-    dedupe: ['d3-transition', 'd3-selection', 'd3-graphviz', 'd3-ease', 'd3-timer', 'd3-interpolate', 'd3-color', 'd3-dispatch'],
-  },
   build: {
     outDir: "build",
     sourcemap: mode === "development" ? "inline" : false,
